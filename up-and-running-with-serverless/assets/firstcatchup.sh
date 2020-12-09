d4c589ecb9569dc0008cf96b3f811571d21e0036
hghotra <ghotra.harvinder@gmail.com>
2020-12-08 21:55:47 -0500

    Revert "Update references to dash"
    This reverts commit da8d55746a5b771657b5be21960dbc6e568b7fe9.

--- up-and-running-with-serverless/assets/firstcatchup.sh
+++ up-and-running-with-serverless/assets/firstcatchup.sh
@@ -12,7 +12,7 @@ rm create-image-upload-url.js
 rm package.json
 echo Creating Serverless.yml file
 cat > serverless.yml <<EOL
-service: dd-serverless-workshop
+service: dash-serverless-workshop
 
 provider:
   name: aws
@@ -111,7 +111,7 @@ echo Creating package.json
 
 cat > package.json <<EOL
 {
-  "name": "dd-serverless-workshop",
+  "name": "dash-serverless-workshop",
   "version": "1.0.0",
   "description": "Demo Image Processing Pipeline in Lambda",
   "main": "index.js",
